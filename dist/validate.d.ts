fca31fbf1594865d834e36d05f6814840b1cefa9
johnnyreilly <johnny_reilly@hotmail.com>
2024-01-01 13:18:19 +0000

    feat: wording

--- dist/validate.d.ts
+++ dist/validate.d.ts
@@ -1,9 +1,7 @@
 import { type ValidationResult } from "./validationResult.js";
 export declare function validateUrl(url: string): Promise<ValidationResult>;
 export interface ProcessedValidationResult {
-	success: boolean;
-	resultText: string;
+    success: boolean;
+    resultText: string;
 }
-export declare function processValidationResult(
-	validationResult: ValidationResult,
-): ProcessedValidationResult;
+export declare function processValidationResult(validationResult: ValidationResult): ProcessedValidationResult;
